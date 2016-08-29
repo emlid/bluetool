6803334d5c262c980d3896c6bfc01a21038fffd5
Aleksandr Aleksandrov <aleksandr.aleksandrov@emlid.com>
2016-08-29 19:53:07 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bluetool',
-    version='0.1.0',
+    version='0.1.1',
     license='BSD',
     author='Aleksandr Aleksandrov',
     author_email='aleksandr.aleksandrov@emlid.com',
