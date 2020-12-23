5adfef654eeeebf9d1f12bee8ccfac533744891e
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2020-12-23 13:28:42 +0300

    setup.py: bump to v0.4.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='bluetool',
-    version='0.3.0',
+    version='0.4.0',
     license='GPL',
     author='Aleksandr Aleksandrov',
     author_email='aleksandr.aleksandrov@emlid.com',
     url='https://github.com/emlid/bluetool',
     packages=find_packages(),
-    install_requires=['tcpbridge']
+    install_requires=['tcpbridge', 'dbus-python', 'pygobject']
 )
